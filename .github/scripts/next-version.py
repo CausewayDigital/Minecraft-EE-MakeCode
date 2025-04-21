ed26acbbf817286f53aa186e29961699e60b16b3
Vincent Lee <vlee@vlee.me.uk>
2025-04-21 16:43:22 +0100

    AZNB-1220 Update release workflow
    Move querying github api into python script

--- .github/scripts/next-version.py
+++ .github/scripts/next-version.py
@@ -1,18 +1,27 @@
 """
 Generate the next version of release for github action
 """
-import os
 import logging
 import json
+import urllib.request
 
-current_release_data = os.getenv("prior_release_data")
-if not current_release_data:
-    logging.error("No data found in env `fetch-api-data`")
-    exit(1)
+request = urllib.request.Request("https://api.github.com/repos/CausewayDigital/Minecraft-EE-MakeCode/releases?per_page=1&page=1",
+                       headers={"accept": "application/vnd.github+json", "X-GitHub-Api-Version":"2022-11-28"})
 
-current_release_data = json.loads(current_release_data)
-version: str = current_release_data[0]["name"]
-split_version = version.split(".")
-new_version = f"{split_version[0]}.{split_version[1]}.{int(split_version[2]) + 1}"
-logging.info(f"new version: {new_version}")
-print(new_version)
+try:
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        json_body = json.loads(body)
+except:
+    logging.error("Unable to make request for release data to Github")
+    print("ERROR")
+
+try:
+    version: str = json_body[0]["name"]
+    split_version = version.split(".")
+    new_version = f"{split_version[0]}.{split_version[1]}.{int(split_version[2]) + 1}"
+    logging.info(f"new version: {new_version}")
+    print(new_version)
+except:
+    logging.error("Unable to calculate next release")
+    print("ERROR")
