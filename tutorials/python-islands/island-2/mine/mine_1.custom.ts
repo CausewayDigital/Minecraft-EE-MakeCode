798cf998763c5296b885061c3b0a9a0a806514c7
Vincent Lee <vlee@vlee.me.uk>
2025-04-21 13:45:24 +0100

    AZNB-1169 Update json

---
+++ tutorials/python-islands/island-2/mine/mine_1.custom.ts
@@ -0,0 +1,29 @@
+/**
+ * Functions for working in the mine
+ */
+namespace agent {
+    /**
+    * Checks if the ground below the agent is stable
+    */
+    //% block="agent check ground stable"
+    export function check_ground_stable(): boolean{
+        const agent_pos = agent.getPosition()
+        if (positions.equals(agent_pos, world(88, 146, 167))){
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Alert ground bellow agent is unstable
+     */
+    //% block
+    export function alert(){
+        if(agent.check_ground_stable()){
+            player.say("Unstable ground alert!!!")
+            agent.destroy(DOWN)
+        }else{
+            player.say("This seems to be a false alarm, this ground seems pretty stable...")
+        }
+    }
+}
