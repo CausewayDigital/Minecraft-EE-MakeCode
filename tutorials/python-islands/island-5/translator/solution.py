a6a003d160d802185645ea9766c5ec372a4ef39c
Vincent Lee <vlee@vlee.me.uk>
2025-05-26 22:16:49 +0100

    Island5 translator solution

---
+++ tutorials/python-islands/island-5/translator/solution.py
@@ -0,0 +1,9 @@
+def decode(info: str):
+    if info == "a":
+        return 1
+    elif info == "b":
+        return 2
+    elif info == "c":
+        return 3
+    elif info == "d":
+        return 4
