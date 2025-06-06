6dd06b20a2ca425ce75ea593b78df7c90b16b391
George Turner <georgeturner308@icloud.com>
2025-06-06 21:01:07 +0100

    comments added to the island 1 mine solution

--- tutorials/python-islands/island-1/mine/solution_mine_1.py
+++ tutorials/python-islands/island-1/mine/solution_mine_1.py
@@ -1,12 +1,15 @@
+# Moves the agent to and destroys the first piece of coal.
 agent.move(UP, 2)
 agent.move(LEFT, 1)
 agent.move(FORWARD, 1)
 agent.destroy(FORWARD)
 
+# From in front of the first piece of coal, moves the agent in front of, and destroys, the second piece of coal.
 agent.move(RIGHT, 2)
 agent.move(DOWN, 1)
 agent.destroy(FORWARD)
 
+# Third piece of coal.
 agent.move(DOWN, 2)
 agent.move(FORWARD, 1)
 agent.destroy(FORWARD)
