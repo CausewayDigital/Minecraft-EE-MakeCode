ef6c20fc269047649621f1b4a852548a58bd46be
George Turner <georgeturner308@icloud.com>
2025-06-06 20:31:57 +0100

    Comments added to solution file to explain where the agent needs to start from for the solution to work for, island 1 lake

--- tutorials/python-islands/island-1/lake/solution_lake_1.py
+++ tutorials/python-islands/island-1/lake/solution_lake_1.py
@@ -1,3 +1,5 @@
+# This is the correct solution for the agent starting at the start point.
+# You can reset your agent by talking to the npc.
 agent.move(FORWARD, 3)
 agent.move(UP, 1)
 agent.move(RIGHT, 5)
--- tutorials/python-islands/island-1/lake/solution_lake_2.py
+++ tutorials/python-islands/island-1/lake/solution_lake_2.py
@@ -1,3 +1,4 @@
+# This is the correct solution for your agent starting at the end point of the previous task.
 agent.move(LEFT, 7)
 agent.move(BACK, 1)
 agent.move(DOWN, 1)
