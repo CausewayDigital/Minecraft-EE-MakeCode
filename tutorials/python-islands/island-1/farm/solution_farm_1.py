aab6fe9735ed6dcacf5b8819051b3d4a2ee727c4
George Turner <georgeturner308@icloud.com>
2025-06-06 20:38:53 +0100

    Updated the island 1 farm solution file with some comments

--- tutorials/python-islands/island-1/farm/solution_farm_1.py
+++ tutorials/python-islands/island-1/farm/solution_farm_1.py
@@ -1,12 +1,15 @@
+# This moves to, tills, and places the seeds for the first point.
 agent.move(LEFT, 3)
 agent.till(DOWN)
 agent.place(DOWN)
 
+# This does the second point assuming the agent starts at the first point.
 agent.move(RIGHT, 3)
 agent.move(FORWARD, 1)
 agent.till(DOWN)
 agent.place(DOWN)
 
+# This does the third and final point assuming the agent starts at the second point.
 agent.move(RIGHT, 4)
 agent.move(FORWARD, 1)
 agent.till(DOWN)
