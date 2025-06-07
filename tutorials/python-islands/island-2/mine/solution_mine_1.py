ceb5a23b2b9d727fc7bc20501948fb0966f766c8
George Turner <georgeturner308@icloud.com>
2025-06-07 14:17:31 +0100

    Updated island 2 mine 1 solution with comment and matched solution to instructions

--- tutorials/python-islands/island-2/mine/solution_mine_1.py
+++ tutorials/python-islands/island-2/mine/solution_mine_1.py
@@ -1,23 +1,5 @@
-agent.move(RIGHT, 1)
-for i in range(3):
-    if agent.check_ground_unstable():
-        agent.alert()
-    agent.move(FORWARD, 1)
+# Run multiple times until agent detects unstable ground
 if agent.check_ground_unstable():
-        agent.alert()
-agent.move(LEFT, 1)
+    agent.alert()
+agent.move(FORWARD, 1)
 
-for i in range(3):
-    if agent.check_ground_unstable():
-        agent.alert()
-    agent.move(BACK, 1)
-if agent.check_ground_unstable():
-        agent.alert()
-agent.move(LEFT, 1)
-
-for i in range(3):
-    if agent.check_ground_unstable():
-        agent.alert()
-    agent.move(FORWARD, 1)
-if agent.check_ground_unstable():
-        agent.alert()
