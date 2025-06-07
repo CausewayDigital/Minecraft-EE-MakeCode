015e20ca46379c5d27e51ac5dcc9ba916321040a
George Turner <georgeturner308@icloud.com>
2025-06-07 14:42:22 +0100

    Added island 2 mine 2 solution

---
+++ tutorials/python-islands/island-2/mine/solution_mine_2.py
@@ -0,0 +1,6 @@
+# Walk over to an entrace of a mineshaft (use shift to crouch and go into it)
+# Call your agent using your whistle
+# Run the code bellow (and repeat for all the other mineshafts
+agent.move(FORWARD, 4)
+if agent.inspect(AgentInspection.BLOCK, FORWARD) == IRON_ORE:
+    agent.destroy(FORWARD)
