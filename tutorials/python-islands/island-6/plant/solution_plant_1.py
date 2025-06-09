7c7cc8229135a4aa47a99363fb8bd4716f4743b8
George Turner <georgeturner308@icloud.com>
2025-06-09 17:25:53 +0100

    island 6 plant solution updated with comment and made to match tutorial

--- tutorials/python-islands/island-6/plant/solution_plant_1.py
+++ tutorials/python-islands/island-6/plant/solution_plant_1.py
@@ -1,5 +1,6 @@
+# Run 3 times, once for each plant. The agent will move automaticall when you submit the correct info.
 block = agent.inspect(AgentInspection.BLOCK, DOWN)
-player.say(block)
+player.say(blocks.name_of_block(block))
 
 hydration = scientist.get_hydration(block)
 nutrition = scientist.get_nutrition(block)
@@ -8,3 +9,4 @@ strength = scientist.get_strength(block)
 plant_info = [block, hydration, nutrition, strength]
 
 scientist.submit(plant_info)
+
