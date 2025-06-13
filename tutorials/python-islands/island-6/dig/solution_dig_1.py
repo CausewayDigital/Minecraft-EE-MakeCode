62ef7deaa02216ac98f9ad6692fb84aa1f485f43
George Turner <georgeturner308@icloud.com>
2025-06-13 14:51:35 +0100

    Changed solution for island 6 dig

--- tutorials/python-islands/island-6/dig/solution_dig_1.py
+++ tutorials/python-islands/island-6/dig/solution_dig_1.py
@@ -1,11 +1,11 @@
 coordinates = []
 
 for row in range(0, 4):
-    for col in range(0,4):
-        player.say(row)
-        player.say(col)
+    for col in range(0, 4):
         if agent.is_fossil_bellow():
             coordinates.append(agent.get_position())
+        player.say(row)
+        player.say(col)
         agent.move(FORWARD, 1)
     agent.move(BACK, 3)
     agent.move(LEFT, 1)
